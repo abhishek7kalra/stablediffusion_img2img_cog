197953f580695d74aa47aa0793fad0172447d5bc
abhishek7kalra <abhishek7.kalra@gmail.com>
2023-07-30 21:00:16 +0530

    Return type data url

--- predict.py
+++ predict.py
@@ -6,6 +6,8 @@ from diffusers import StableDiffusionImg2ImgPipeline
 from torch.utils.data import Dataset
 import os
 import random
+import io
+import base64
 
 
 MODEL_ID = "runwayml/stable-diffusion-v1-5"
@@ -16,32 +18,43 @@ class Predictor(BasePredictor):
         print("Loading pipeline...")
         self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
             MODEL_ID,
-            torch_dtype=torch.float32,
-            #float16
+            torch_dtype=torch.float16,
             cache_dir=MODEL_CACHE,
             local_files_only=True,
         ).to("cuda")
         
         self.pipe.enable_attention_slicing()
-        #remove slicing
 
     @torch.inference_mode()
     def predict(
         self,
         input_photo: Path = Input(description="Path to the input photo"),
-        #output_path: Path = Input(description="Path to save the output styled image"),
         prompt: str = Input(description="Style prompt", default="Van Gogh style"),
     ) -> Path:
+        
         input_photo_path = input_photo
-        #output_image_path = output_path
+        
         print("testing")
         init_img = Image.open(input_photo_path)
-        init_img = init_img.resize((768, 512))
+        init_img = init_img.resize((512, 512))
 
         output = self.pipe(prompt=prompt, image=init_img, strength=0.75, guidance_scale=7.5).images
-        print("saving")
-        output_image_path = "output.png"  # Temporarily save the image
-        output[0].save(output_image_path)
+        
+        print("saving...")
+        output_path = "output.png" 
+    
+        with io.BytesIO() as output_buffer:
+            output[0].save(output_buffer, format='PNG')
+            output_bytes = output_buffer.getvalue()
 
-        return output_image_path
+        # Create a data URL from the output bytes
+        data_url = "data:image/png;base64," + base64.b64encode(output_bytes).decode()
+        
+        # Saving the image
+        with open(output_path, 'wb') as output_file:
+            output_file.write(base64.b64decode(data_url.split(",")[1]))
+            print("saved")
+        
+        # Return the data URL as the output prediction
+        return data_url
 
